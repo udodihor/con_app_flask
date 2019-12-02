227cf95fb9955aefcf7a8e858f404194504cf9b9
Ihor <ihor.udod@elpassion.pl>
2019-12-02 22:24:22 +0100

    Photo Update added

--- fl_blog/routes.py
+++ fl_blog/routes.py
@@ -1,3 +1,5 @@
+import os
+import secrets
 from flask import render_template, url_for, flash, redirect, request
 from fl_blog import app, db, bcrypt
 from fl_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -63,6 +65,16 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+# Save User profile picture:
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
 
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
