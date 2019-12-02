d18b1ddc7b8e45732ce9049bdf8770d0d30ee6a4
Ihor <ihor.udod@elpassion.pl>
2019-12-02 22:10:14 +0100

    File Validation added

--- fl_blog/forms.py
+++ fl_blog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -19,9 +20,10 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken.')
+    
     def validate_email(self,email):
-        email = User.query.filter_by(email=email.data).first()
-        if email:
+        user = User.query.filter_by(email=email.data).first()
+        if user:
             raise ValidationError('That email is already taken.')
 
 
@@ -39,15 +41,16 @@ class UpdateAccountForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is taken.')
+            if user:
+                raise ValidationError('That username is taken.')
     def validate_email(self,email):
         if email.data != current_user.email:
-            email = User.query.filter_by(email=email.data).first()
-            if email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
                 raise ValidationError('That email is already taken.')
--- fl_blog/routes.py
+++ fl_blog/routes.py
@@ -41,7 +41,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('registration.html', title='Register', form=form)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
@@ -55,31 +55,31 @@ def login():
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check your email or password.', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account', methods=['GET', 'POST'])
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
-        
-        # Saving new data to the Data Base:
         db.session.commit()
-        flash('Your account has been updated', 'success')
+        flash('Your account has been updated!', 'success')
         return redirect(url_for('account'))
-    
-    # Show current user data (email and username):
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Accout', image_file=image_file, form=form)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
